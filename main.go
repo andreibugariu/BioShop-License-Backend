b62b62a9fb32e622144b2d9588202f329744c21d
andrei <andrei.bugariu2@gmail.com>
2022-03-06 12:47:17 +0200

    Create CRUD APIs for User

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -63,6 +64,107 @@ func main() {
 	}*/
 
 	router := mux.NewRouter()
+	
+
+	router.HandleFunc("/users", GetUsers).Methods("GET")//merge
+	router.HandleFunc("/user/{id}", GetUser).Methods("GET")//merge
+	router.HandleFunc("/user", CreateUser).Methods("POST")//merge
+	router.HandleFunc("/user/{id}", DeleteUser).Methods("DELETE")//merge
+	router.HandleFunc("/user/{id}", UpdateUser).Methods("PUT")
+
+
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+
+//Create a new user
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	var user entity.User
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	createUser := db.Create(&user)
+	err = createUser.Error
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(&user)
+	}
+}
+
+
+//Get all  users
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+
+
+	var user []entity.User
+	result := db.Find(&user)
+	if result.RecordNotFound() {
+		http.Error(w, "Not fount", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(&user)
 
 }
+
+
+//Get specific user and their rentals
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	//Check the credentials provided in the request. Also check for errors at authentication.
+
+	params := mux.Vars(r)
+
+	var user entity.User
+	var orders []entity.Order
+	result := db.Where("id = ?", params["id"]).First(&user)
+	if result.RecordNotFound() {
+		http.Error(w, "Not fount", http.StatusNotFound)
+		return
+	}
+
+	db.Model(&user).Related(&orders)
+
+	user.Orders = orders
+
+	json.NewEncoder(w).Encode(user)
+}
+
+//Delete a specific user by ID
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+
+	var user entity.User
+
+	result := db.Where("id = ?", params["id"]).First(&user)
+	if result.RecordNotFound() {
+		http.Error(w, "Not fount", http.StatusNotFound)
+		return
+	}
+
+	result = db.Delete(&user)
+	if result.Error != nil {
+		http.Error(w, "can't delete users", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode("Userul is succefully deleting")
+}
+
+//Update a specific user by ID
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	
+	var user entity.User
+	params := mux.Vars(r)
+	json.NewDecoder(r.Body).Decode(&user)
+
+	result := db.Model(&entity.User{}).Where("id= ?", params["id"]).Updates(user)
+	if result.Error != nil {
+		http.Error(w, "Can't update", http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode("Userul is succefully UPTDATE")
+}
+
