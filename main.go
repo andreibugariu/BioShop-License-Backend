9729e992aa3e7c23fcb9c4a43d69f5954b96e6c9
andrei <andrei.bugariu2@gmail.com>
2022-05-28 11:49:51 +0300

    Add Login and Logout using jwt

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/andreibugariu/BioShop-License/entity"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/sirupsen/logrus"
 )
 
 /*var (
@@ -97,7 +102,138 @@ func main() {
 	router.HandleFunc("/order_product/{id}", DeleteOrderProduct).Methods("DELETE")//merge
 	router.HandleFunc("/order_product/{id}", UpdateOrderProduct).Methods("PUT")//merge
 
+	router.HandleFunc("/login", Login)//merge
+	//router.HandleFunc("/home", Home).Methods("GET")
+	router.HandleFunc("/logout", DeleteCookie)//merge
+
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+///Login
+var jwtKey = []byte("secret_key")
+
+
+//Encode uses base64 as main encoding method.
+func Encode(input string) string {
+	return base64.StdEncoding.EncodeToString([]byte(input))
+}
+
+//HasError uses logger to log the error if any has occured.
+func HasError(err error, message string) bool {
+
+	if err != nil {
+		logrus.WithError(err).Error(message)
+		return true
+	}
+
+	return false
+}
+
+type Claims struct {
+	Email string `json:"username"`
+	jwt.StandardClaims
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	reqbody := r.Body
+	bodyBytes, err := ioutil.ReadAll(reqbody)
+
+	if HasError(err,"Internal Error. Unable to read data") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+
+	var user entity.User
+	err = json.Unmarshal(bodyBytes, &user)
+
+	if HasError(err,"Internal Error. Unmarshal problem") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+
+	var userDB entity.User
+	result := db.Find(&userDB, "email=?", user.Email)
+
+	if result.RecordNotFound() {
+		http.Error(w, "Email does not exist", http.StatusUnauthorized)
+		return
+	}
+
+	if user.Password != userDB.Password {
+		http.Error(w, "Incorrect password", http.StatusUnauthorized)
+		return
+	}
+
+	expirationTime := time.Now().Add(time.Minute * 5)
+
+	claims := &Claims{
+		Email: user.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w,
+		&http.Cookie{
+			Name:    "token",
+			Value:   tokenString,
+			Expires: expirationTime,
+		})
+
+}
+
+func IsAuth(w http.ResponseWriter, r *http.Request)  (bool, error) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return false, err
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return false, err
+	}
+
+	tokenStr := cookie.Value
+
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return false, err
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return false, err
+	}
+
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return false, err
+	}
+	
+	return true, nil
+}
+
+
+func DeleteCookie(w http.ResponseWriter, r *http.Request) {
+	c := http.Cookie{
+			Name:   "token",
+			MaxAge: -1}
+	http.SetCookie(w, &c)
+
+	w.Write([]byte("old cookie deleted!\n"))
 }
 
 /////////////// API for USER
@@ -120,8 +256,13 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 
 //Get all  users
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-
-
+    
+	isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var user []entity.User
 	result := db.Find(&user)
 	if result.RecordNotFound() {
@@ -130,6 +271,7 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(&user)
+    }
 
 }
 
@@ -137,7 +279,12 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 //Get specific user and their rentals
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	//Check the credentials provided in the request. Also check for errors at authentication.
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	params := mux.Vars(r)
 
 	var user entity.User
@@ -154,10 +301,16 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(user)
 }
+}
 
 //Delete a specific user by ID
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	params := mux.Vars(r)
 
 	var user entity.User
@@ -176,10 +329,16 @@ func DeleteUser(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode("Userul is succefully deleting")
 }
+}
 
 //Update a specific user by ID
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	
+	isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var user entity.User
 	params := mux.Vars(r)
 	json.NewDecoder(r.Body).Decode(&user)
@@ -190,6 +349,7 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	json.NewEncoder(w).Encode("Userul is succefully UPTDATE")
+}
 }
 
 /////////////// API for FARMERS
@@ -213,7 +373,12 @@ func CreateFarmer(w http.ResponseWriter, r *http.Request) {
 //Get all  farmers
 func GetFarmers(w http.ResponseWriter, r *http.Request) {
 
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var farmer []entity.Farmer
 	result := db.Find(&farmer)
 	if result.RecordNotFound() {
@@ -222,6 +387,7 @@ func GetFarmers(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(&farmer)
+}
 
 }
 
@@ -229,7 +395,12 @@ func GetFarmers(w http.ResponseWriter, r *http.Request) {
 //Get specific farmer and their products
 func GetFarmer(w http.ResponseWriter, r *http.Request) {
 	//Check the credentials provided in the request. Also check for errors at authentication.
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	params := mux.Vars(r)
 
 	var farmer entity.Farmer
@@ -246,10 +417,16 @@ func GetFarmer(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(farmer)
 }
+}
 
 //Delete a specific user by ID
 func DeleteFarmer(w http.ResponseWriter, r *http.Request) {
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	params := mux.Vars(r)
 
 	var farmer entity.Farmer
@@ -268,10 +445,16 @@ func DeleteFarmer(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode("Farmer is succefully deleting")
 }
+}
 
 //Update a specific user by ID
 func UpdateFarmer(w http.ResponseWriter, r *http.Request) {
-	
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var farmer entity.Farmer
 	params := mux.Vars(r)
 	json.NewDecoder(r.Body).Decode(&farmer)
@@ -283,12 +466,18 @@ func UpdateFarmer(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode("The Farmer is succefully UPTDATE")
 }
+}
 
 
 /////////////// API for PRODUCT
 
 func CreateProduct(w http.ResponseWriter, r *http.Request) {
-
+	isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var product entity.Product
 
 	json.NewDecoder(r.Body).Decode(&product)
@@ -301,10 +490,16 @@ func CreateProduct(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(&product)
 	}
 }
+}
 
 func GetProducts(w http.ResponseWriter, r *http.Request) {
 
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var product []entity.Product
 	result := db.Find(&product)
 	if result.RecordNotFound() {
@@ -313,6 +508,7 @@ func GetProducts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(&product)
+}
 
 }
 
@@ -320,7 +516,12 @@ func GetProducts(w http.ResponseWriter, r *http.Request) {
 
 func GetProduct(w http.ResponseWriter, r *http.Request) {
 	//Check the credentials provided in the request. Also check for errors at authentication.
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	params := mux.Vars(r)
 
 	var product entity.Product
@@ -337,10 +538,16 @@ func GetProduct(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(product)
 }
+}
 
 
 func DeleteProduct(w http.ResponseWriter, r *http.Request) {
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	params := mux.Vars(r)
 
 	var product entity.Product
@@ -359,9 +566,15 @@ func DeleteProduct(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode("Product is succefully deleting")
 }
+}
 
 func UpdateProduct(w http.ResponseWriter, r *http.Request) {
-	
+	isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var product entity.Product
 	params := mux.Vars(r)
 	json.NewDecoder(r.Body).Decode(&product)
@@ -373,11 +586,17 @@ func UpdateProduct(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode("Product is succefully UPTDATE")
 }
+}
 
 
 /////////////// API for ORDERS
 func CreateOrder(w http.ResponseWriter, r *http.Request) {
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var order entity.Order
 
 	json.NewDecoder(r.Body).Decode(&order)
@@ -390,12 +609,18 @@ func CreateOrder(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(&order)
 	}
 }
+}
 
 
 //Get all  order
 func GetOrders(w http.ResponseWriter, r *http.Request) {
 
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var order []entity.Order
 	result := db.Find(&order)
 	if result.RecordNotFound() {
@@ -404,6 +629,7 @@ func GetOrders(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(&order)
+}
 	
 
 }
@@ -413,6 +639,12 @@ func GetOrders(w http.ResponseWriter, r *http.Request) {
 func GetOrder(w http.ResponseWriter, r *http.Request) {
 	//Check the credentials provided in the request. Also check for errors at authentication.
     //Get order and the product
+	isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	params := mux.Vars(r)
 
 	var order entity.Order
@@ -428,10 +660,16 @@ func GetOrder(w http.ResponseWriter, r *http.Request) {
 	order.Orders_Products = orders_products
 	json.NewEncoder(w).Encode(order)
 }
+}
 
 //Delete a specific order by ID
 func DeleteOrder(w http.ResponseWriter, r *http.Request) {
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	params := mux.Vars(r)
 
 	var order entity.Order
@@ -450,10 +688,16 @@ func DeleteOrder(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode("The order is succefully deleting")
 }
+}
 
 //Update a specific order by ID
 func UpdateOrder(w http.ResponseWriter, r *http.Request) {
-	
+	isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var order entity.Order
 	params := mux.Vars(r)
 	json.NewDecoder(r.Body).Decode(&order)
@@ -465,10 +709,16 @@ func UpdateOrder(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode("Order is succefully UPTDATE")
 }
+}
 
 /////////////// API for ORDERS_PRODUCT
 func CreateOrderProduct(w http.ResponseWriter, r *http.Request) {
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var order_product entity.Orders_Products
 
 	json.NewDecoder(r.Body).Decode(&order_product)
@@ -481,12 +731,18 @@ func CreateOrderProduct(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(&order_product)
 	}
 }
+}
 
 
 //Get all  products
 func GetOrdersProducts(w http.ResponseWriter, r *http.Request) {
 
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var order_product []entity.Orders_Products
 	result := db.Find(&order_product)
 	if result.RecordNotFound() {
@@ -495,6 +751,7 @@ func GetOrdersProducts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(&order_product)
+}
 
 }
 
@@ -502,7 +759,12 @@ func GetOrdersProducts(w http.ResponseWriter, r *http.Request) {
 //Get specific user and their rentals
 func GetOrderProduct(w http.ResponseWriter, r *http.Request) {
 	//Check the credentials provided in the request. Also check for errors at authentication.
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	params := mux.Vars(r)
 
 	var order_product entity.Orders_Products
@@ -514,10 +776,16 @@ func GetOrderProduct(w http.ResponseWriter, r *http.Request) {
      
 	json.NewEncoder(w).Encode(order_product)
 }
+}
 
 //Delete a specific product by ID
 func DeleteOrderProduct(w http.ResponseWriter, r *http.Request) {
-
+    isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	params := mux.Vars(r)
 
 	var order_product entity.Orders_Products
@@ -536,10 +804,16 @@ func DeleteOrderProduct(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode("The order_product is succefully deleting")
 }
+}
 
 //Update a specific product by ID
 func UpdateOrderProduct(w http.ResponseWriter, r *http.Request) {
-	
+	isAuth, err := IsAuth(w, r)
+	if HasError(err,"Error in authentication function") {
+		http.Error(w,"Internal Error. Please try again later", http.StatusInternalServerError)
+		return
+	}
+	if isAuth {
 	var order_product entity.Orders_Products
 	params := mux.Vars(r)
 	json.NewDecoder(r.Body).Decode(&order_product)
@@ -551,3 +825,34 @@ func UpdateOrderProduct(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode("succefully UPTDATE")
 }
+}
+
+//var productsID []string
+	//for _, num := range orders_products. {
+	//    productsID = append(productsID,num)
+	//  }
+	/*var products []entity.Product
+
+	    for _, productID := range id {
+			var product entity.Product
+
+			result := db.Where("id = ?", productID).First(&product)
+		     if result.RecordNotFound() {
+			  http.Error(w, "Not fount", http.StatusNotFound)
+			  return
+	  	   }
+		 products := append(products, product)
+
+	     }
+
+		   ASA TREBUIE FACUT SI AICI
+
+		ceva := []string{"Python", "Java", "C#", "Go", "Ruby"}
+		var altceva[]string
+		for _, a := range ceva{
+		 var b string
+		 b=a;
+		 altceva =append(altceva,b)
+		}
+		fmt.Print(altceva)
+	*/
